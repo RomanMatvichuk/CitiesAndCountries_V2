3cd5ed4212560b73d449d9da73f3c4619f468aa8
Roman Matvichuk <maro19xr@student.ju.se>
2019-09-29 23:07:24 +0200

    Finished

--- script.js
+++ script.js
@@ -1,25 +1,69 @@
-getCities();
-getCountries();
+var visitedCities = [];
+var metPeople = 0;
+var cityData = "";
+var countryData = "";
 
-async function getCities() {
-    var cities = await fetch('stad.json')
-        .then(response => response.json())
-        .catch(error => document.write(error));
+getList();
+
+async function getList() {
+        var cities = await fetch('stad.json')
+            .then(response => response.json())
+            .catch(error => document.write(error));
+        cities.sort((a, b) => a.countryid - b.countryid  || b.population - a.population);
+        cityData = JSON.stringify(cities);
+
+        var countries = await fetch('land.json')
+            .then(response => response.json())
+            .catch(error => document.write(error));
+        countries.sort((a, b) => (a.countryname < b.countryname) ? -1 : 1);
+        countryData = JSON.stringify(countries);
+
+    if (localStorage.getItem("visitedCities") !== null){
+        visitedCities = JSON.parse(localStorage.getItem("visitedCities"));
+    }    
+
+    if (localStorage.getItem("metPeople") !== null){
+        metPeople = parseInt(localStorage.getItem("metPeople"));
+        metPeopleRaport();
+    }
     
-    cities.sort((a, b) => a.countryid - b.countryid  || b.population - a.population);
-    localStorage.setItem("cities", JSON.stringify(cities));
+    var cities = await JSON.parse(cityData);
+    var countries = await JSON.parse(countryData);
+    
+    var htmlLines = "";
+
+    for (var i = 0; i < countries.length; i++) {
+        htmlLines += "<li><a href=\"javascript:dropDownList(\'l_" + countries[i].id + "\');\">" + countries[i].countryname + "</a></li>\n";
+        htmlLines += "<li><ul id='l_" + countries[i].id + "' style='display: none'>\n";
+        
+        for (var j = 0; j < cities.length; j++) {
+            if (countries[i].id === cities[j].countryid){
+                htmlLines += "<li><a href=\"javascript:dropDownList(\'" + cities[j].id + "\');\">" + cities[j].stadname + "</a></li>\n";
+                htmlLines += "<li><div id='" + cities[j].id + "' class='info space' style='display: none'>\n";
+                htmlLines += "<p><strong>" + cities[j].stadname + "</strong></p>\n";
+                htmlLines += "<p>" + cities[j].population + " invånare</p>\n";
+                htmlLines += "<input id=\"b_" + cities[j].id + "\" type=\"checkbox\" value=\"true\" ";
+                htmlLines += "onclick=\"visitCity(" + cities[j].id + ", \'" + cities[j].stadname + " (" + countries[i].countryname + ") " + "\')\">\n"
+                htmlLines += "<label for='b_" + cities[j].id + "'>Besökt</label></div></li>\n";
+            }            
+        } 
+
+        htmlLines += "</ul></li>";
+    }
+
+    document.getElementById("lands").innerHTML = htmlLines;
+
+    if (visitedCities.length !== 0){
+        for (var i = 0; i < visitedCities.length; i++){
+            var city = getCityData(visitedCities[i].id);
+            document.getElementById("b_" + city.id).checked = true;
+            addListItem("vCities", "v_" + city.id, city.stadname + " (" + city.population + " invånare)");
+        }
+    }
+
+    checkVisitedCities();
 }
 
-async function getCountries() {
-    var countries = await fetch('land.json')
-        .then(response => response.json())
-        .catch(error => document.write(error));
-    
-    countries.sort((a, b) => (a.countryname < b.countryname) ? -1 : 1);
-    localStorage.setItem("countries", JSON.stringify(countries));
-}
-
-//console.log(stad, land)
 
 function dropDownList(id){
     var ul = document.getElementById(id);
@@ -32,15 +76,38 @@ function dropDownList(id){
     }
 }
 
-function visitCity(id){
-    //var ul = document.getElementById("vCities");
+function visitCity(id, name){
     var cb = document.getElementById("b_" + id);
+    var peopleQty = getCityData(id).population;
     
     if (cb.checked === true){
-        addListItem("vCities", "v_" + id, "s" + id);
+        addListItem("vCities", "v_" + id, name + " (" + peopleQty + " invånare)");
+        metPeople += peopleQty;
+        var object = {id: id};
+        visitedCities.push(object);        
     }
     else {
         removeChildElement("vCities", "v_" + id);
+        metPeople -= peopleQty;
+        for (var i = 0; i < visitedCities.length; i++){
+            if (visitedCities[i].id === id){
+                visitedCities.splice(i, 1);
+            }
+        }
+    }
+
+    metPeopleRaport();
+    saveVisitedInfo();
+    checkVisitedCities();
+}
+
+function getCityData(id){
+    var data = JSON.parse(cityData);
+    for (var i = 0; i < data.length; i++) {
+        if (data[i].id === id) {
+            var object = {id: data[i].id, stadname: data[i].stadname, countryid: data[i].countryid, population: data[i].population};
+            return object;
+        }
     }
 }
 
@@ -50,10 +117,69 @@ function addListItem(ulId, liId, text){
     li.setAttribute("id", liId);
     li.innerText = text;
     ul.appendChild(li);
+    sortList();
 }
 
 function removeChildElement(parentId, childId){
     var parent = document.getElementById(parentId);
     var child = document.getElementById(childId);
     parent.removeChild(child);
+    sortList();
 }
+
+function saveVisitedInfo (){
+    localStorage.setItem("metPeople", metPeople);
+    localStorage.setItem("visitedCities", JSON.stringify(visitedCities));
+}
+
+function metPeopleRaport(){
+    document.getElementById("metPeople").innerText = "Träffades " + metPeople + " människor";
+}
+
+function checkVisitedCities(){
+    if (visitedCities.length === 0){
+        document.getElementById("visited").style.display = "none";
+    }
+    else{
+        document.getElementById("visited").style.display = "block";
+    }
+}
+
+function emtyData(){
+    localStorage.clear();
+    location.reload(true);
+}
+
+// W3Schools listsorteringsfunktion
+function sortList(id) {
+    var list, i, switching, b, shouldSwitch;
+    list = document.getElementById("vCities");
+    switching = true;
+    /* Make a loop that will continue until
+    no switching has been done: */
+    while (switching) {
+      // start by saying: no switching is done:
+      switching = false;
+      b = list.getElementsByTagName("li");
+      // Loop through all list-items:
+      for (i = 0; i < (b.length - 1); i++) {
+        // start by saying there should be no switching:
+        shouldSwitch = false;
+        /* check if the next item should
+        switch place with the current item: */
+        if (b[i].innerHTML.toLowerCase() > b[i + 1].innerHTML.toLowerCase()) {
+          /* if next item is alphabetically
+          lower than current item, mark as a switch
+          and break the loop: */
+          shouldSwitch = true;
+          break;
+        }
+      }
+      if (shouldSwitch) {
+        /* If a switch has been marked, make the switch
+        and mark the switch as done: */
+        b[i].parentNode.insertBefore(b[i + 1], b[i]);
+        switching = true;
+      }
+    }
+  }
